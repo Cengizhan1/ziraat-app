d68d230fe3c7ad28003abcf159cf86e67fa6fdea
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-04-22 01:54:49 +0300

    first commit

---
+++ user/src/main/java/com/ziraat/app/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.ziraat.app.user;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.ziraat.app.card")
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}

3f13b4ee264fde31fb34f98df84fdc254708dfcb
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-04-23 00:30:59 +0300

    date format added

--- user/src/main/java/com/ziraat/app/user/model/UserInformation.java
+++ user/src/main/java/com/ziraat/app/user/model/UserInformation.java
@@ -6,7 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
@@ -27,7 +29,9 @@ public class UserInformation {
     private Integer age;
     private String job;
     private Double salary;
-    private LocalDateTime birthDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthDate;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")

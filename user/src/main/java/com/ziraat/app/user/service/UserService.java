c0b387659d694bd82e57d87369f90ff8706b9f4c
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-04-22 16:23:24 +0300

    UserService created

---
+++ user/src/main/java/com/ziraat/app/user/service/UserService.java
@@ -0,0 +1,20 @@
+package com.ziraat.app.user.service;
+
+
+import com.ziraat.app.user.repository.UserRepository;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+}

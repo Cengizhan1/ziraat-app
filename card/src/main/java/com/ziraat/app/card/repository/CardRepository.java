ed6c0406bd5a3583813d69e308797b1500348e24
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-04-24 17:03:59 +0300

    existsByCardNumber method added CardRepository

--- card/src/main/java/com/ziraat/app/card/repository/CardRepository.java
+++ card/src/main/java/com/ziraat/app/card/repository/CardRepository.java
@@ -4,4 +4,5 @@ import com.ziraat.app.card.model.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
+    boolean existsByCardNumber(String cardNumber);
 }
